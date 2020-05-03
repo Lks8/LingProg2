0299aeb23d654dae8fb83ea4a43cc7ff317992a7
Lks8 <lucas.martins.coelho@hotmail.com>
2020-05-03 18:51:05 -0300

    ex5

---
+++ javascript/jogo/app.js
@@ -0,0 +1,8 @@
+var pontuacao = [0,0];
+var pontuacaoRodada = 0;
+var jogadorAtual = 0;
+
+var dado = Math.floor(Math.random()*6)+1;
+
+document.querySelector("#pontuacao-rodada-0").textContent = dado;
+document.querySelector("#botao-lancar-dado").addEventListener("click",function(){})
---
+++ javascript/script.js
@@ -0,0 +1,34 @@
+
+
+pessoa = {
+    nome: "ana",
+    idade: 30,
+    sexo: "F"
+    endereco:{
+        rua
+    }
+}
+// console.log(pessoa)
+// console.log(pessoa.nome)
+// console.log(pessoa['idade'])
+
+
+// var itens = ["ana","Paula",23,23.2,true]
+
+// var u;
+
+// var n = 2
+// n = null;
+
+// var x = true;
+// var y = false;
+
+// var n1 = 2;
+// var n2 = 2.2;
+
+// var s1 = "Ana";
+// var s2 = 'Maria';
+// // var s3 = `Ela se chama ${s1}`
+// var s4 = 'Aspas duplas "aqui"'
+
+// console.log(typeof(s1));
